afe11e08603a28404dd152ba19be1b16f257e756
sostk <poudelswastik@gmail.com>
2024-11-21 10:16:08 +1100

    Added images

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const slideshow = document.getElementById('slideshow');
 
 // Define your image folder and images
 const imageFolder = 'images/'; // Replace with your folder path
-const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg']; // Replace with actual filenames
+const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpeg', '9.jpeg', '10.jpeg', '11.jpeg']; // Replace with actual filenames
 
 // Add images to the slideshow dynamically
 images.forEach((image) => {
